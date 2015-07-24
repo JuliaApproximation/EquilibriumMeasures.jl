bc8fc2afc0f5a9c5af8de395330bf52c8bca985e
Sheehan Olver <solver@mac.com>
2015-07-24 17:48:12 +1000

    started module

---
+++ src/EquilibriumMeasures.jl
@@ -0,0 +1,44 @@
+module EquilibriumMeasures
+    using Base, Compat, DualNumbers, ApproxFun, SingularIntegralEquations
+
+export equilbriummeasure
+
+# We need to implement some functionality for the ApproxFun constructor to work with dual numbers
+Base.sinpi(x::Dual)=sin(π*x)
+ApproxFun.real{T}(::Type{Dual{T}})=Dual{ApproxFun.real(T)}
+ApproxFun.plan_chebyshevtransform{D<:Dual}(v::Vector{D})=ApproxFun.plan_chebyshevtransform(real(v))
+ApproxFun.chebyshevtransform{D<:Dual}(v::Vector{D},plan...)=dual(chebyshevtransform(real(v),plan...),chebyshevtransform(epsilon(v),plan...))
+ApproxFun.chop!(f::Fun,d::Dual)=chop!(f,real(d))
+
+
+# this must be zero for the corrects support
+function F(V,a,b)
+    f=Fun(V,Interval(a,b))'
+    [f.coefficients[1],(b-a)/8*f.coefficients[2]-1]
+end
+
+
+
+function equilbriummeasuresupport(V,ab=Interval();maxiterations=100)
+    a,b=ab.a,ab.b
+
+    # Newton iteration, using dual numbers
+    for k=1:maxiterations
+        F1=F(V,dual(a,1.),b)
+        p=real(F1)
+        J=[epsilon(F1) epsilon(F(V,a,dual(b,1)))]
+
+        an,bn=[a,b]-J\p
+        if isapprox(an,a) && isapprox(bn,b)
+            return Interval(an,bn)
+        else
+            a,b=an,bn
+        end
+    end
+
+    warn("maxiterations reached")
+    Interval(a,b)
+end
+equilbriummeasure(V,gs...;opts...)=hilbertinverse(Fun(V,equilbriummeasuresupport(V,gs...;opts...))')/(2π)
+
+end #module

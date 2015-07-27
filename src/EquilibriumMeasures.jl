22854a3ae9cec37b2c7ec63387bd3748a5ff92e3
Sheehan Olver <solver@mac.com>
2015-07-27 17:42:21 +1000

    add hilbertinverse, started one-side infinite case

--- src/EquilibriumMeasures.jl
+++ src/EquilibriumMeasures.jl
@@ -11,10 +11,51 @@ ApproxFun.chebyshevtransform{D<:Dual}(v::Vector{D},plan...)=dual(chebyshevtransf
 ApproxFun.chop!(f::Fun,d::Dual)=chop!(f,real(d))
 
 
+
+
+## hilbertinverse
+# returns a function whose hilbert transform is the given function
+
+function hilbertinverse(u::Fun;tolerance=100eps(),bounded=:none)
+    cfs=coefficients(u,Chebyshev)
+    if abs(first(cfs)) < tolerance
+        # no singularity
+        # invert Corollary 5.6 of Olver&Trogdon
+        cfs=-coefficients(cfs[2:end],Ultraspherical{1},Chebyshev)
+        Fun(cfs,JacobiWeight(.5,.5,Chebyshev(domain(u))))
+    else
+        # invert Corollary 5.10 of Olver&Trogdon
+        if bounded==:left
+            cfs=[cfs[1],cfs[1],.5*cfs[2:end]]
+        elseif bounded==:right
+            cfs=[-cfs[1],cfs[1],.5*cfs[2:end]]
+        else
+            cfs=[0.,cfs[1],.5*cfs[2:end]]
+        end
+        cfs=coefficients(cfs,ChebyshevDirichlet{1,1},Chebyshev)
+        Fun(cfs,JacobiWeight(-.5,-.5,Chebyshev(domain(u))))
+    end
+end
+
+
+
 # this must be zero for the corrects support
+
+function emmoment(f::Fun{Chebyshev};bounded=:none)
+    cfs=pad(f.coefficients,2)
+    if bounded==:left
+        (a-b)/4*cfs[1]
+    elseif bounded==:right
+        (b-a)/4*cfs[1]
+    else
+        (b-a)/8*cfs[2]
+    end
+end
+
 function F(V,a,b)
     f=Fun(V,Interval(a,b))'
-    [f.coefficients[1],(b-a)/8*f.coefficients[2]-1]
+
+    [f.coefficients[1],emmoment(f)-1]
 end
 
 

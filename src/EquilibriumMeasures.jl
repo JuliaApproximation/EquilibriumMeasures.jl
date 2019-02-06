2ba036b3c9dc086cb127b9a4cfe3ec71a2ffd1b2
Sheehan Olver <solver@mac.com>
2019-02-06 10:48:18 +0000

    Update for Julia 1.0

--- src/EquilibriumMeasures.jl
+++ src/EquilibriumMeasures.jl
@@ -5,13 +5,6 @@ module EquilibriumMeasures
 
 export equilibriummeasure,hilbertinverse
 
-# We need to implement some functionality for the ApproxFun constructor to work with dual numbers
-
-ApproxFun.real{T}(::Type{Dual{T}})=Dual{ApproxFun.real(T)}
-ApproxFun.plan_chebyshevtransform{D<:Dual}(v::Vector{D})=ApproxFun.plan_chebyshevtransform(realpart(v))
-ApproxFun.chebyshevtransform{D<:Dual}(v::Vector{D},plan...)=dual(chebyshevtransform(realpart(v),plan...),chebyshevtransform(epsilon(v),plan...))
-ApproxFun.chop!(f::Fun,d::Dual)=chop!(f,realpart(d))
-
 
 
 
@@ -23,8 +16,8 @@ function hilbertinverse(u::Fun;tolerance=100eps(),bounded=:none)
     if abs(first(cfs)) < tolerance
         # no singularity
         # invert Corollary 5.6 of Olver&Trogdon
-        cfs=-coefficients(cfs[2:end],Ultraspherical{1},Chebyshev)
-        Fun(cfs,JacobiWeight(.5,.5,Chebyshev(domain(u))))
+        cfs=-coefficients(cfs[2:end], Ultraspherical(1,domain(u)),Chebyshev(domain(u)))
+        Fun(JacobiWeight(.5,.5,Chebyshev(domain(u))),cfs)
     else
         # invert Corollary 5.10 of Olver&Trogdon
         if bounded==:left
@@ -34,8 +27,8 @@ function hilbertinverse(u::Fun;tolerance=100eps(),bounded=:none)
         else
             cfs=[0.;cfs[1];.5*cfs[2:end]]
         end
-        cfs=coefficients(cfs,ChebyshevDirichlet{1,1},Chebyshev)
-        Fun(cfs,JacobiWeight(-.5,-.5,Chebyshev(domain(u))))
+        cfs=coefficients(cfs,ChebyshevDirichlet{1,1}(domain(u)),Chebyshev(domain(u)))
+        Fun(JacobiWeight(-.5,-.5,Chebyshev(domain(u))),cfs)
     end
 end
 
@@ -43,10 +36,10 @@ end
 
 # this must be zero for the corrects support
 
-function emmoment{CC<:Chebyshev}(f::Fun{CC};bounded=:none)
+function emmoment(f::Fun{<:Chebyshev};bounded=:none)
     cfs=pad(f.coefficients,2)
     d=domain(f)
-    a,b=d.a,d.b
+    a,b = endpoints(d)
     if bounded==:left
         (a-b)/4*cfs[1]
     elseif bounded==:right
@@ -57,49 +50,50 @@ function emmoment{CC<:Chebyshev}(f::Fun{CC};bounded=:none)
 end
 
 function F(V,a,b)
-    f=Fun(V,Interval(a,b))'
+    f=Fun(V,a..b)'
 
     [f.coefficients[1],emmoment(f)-1]
 end
 
 
-equilibriummeasuresupport(V,ab::Vector;opts...)=equilibriummeasuresupport(V,convert(Domain,ab);opts...)
-function equilibriummeasuresupport(V,ab=Interval();maxiterations=100,bounded=:none)
+equilibriummeasuresupport(V,ab::Vector;opts...) =
+    equilibriummeasuresupport(V,convert(Domain,ab);opts...)
+function equilibriummeasuresupport(V,ab=(-1.0..1.0);maxiterations=100,bounded=:none)
     if bounded == :left
         return equilibriummeasuresupportbounded(false,V,ab;maxiterations=maxiterations)
     elseif bounded == :right
         return equilibriummeasuresupportbounded(true,V,ab;maxiterations=maxiterations)
     end
 
-    a,b=sort([ab.a,ab.b])
+    a,b = endpoints(ab)
 
     # Newton iteration, using dual numbers
     for k=1:maxiterations
-        F1=F(V,dual(a,1.),b)
-        p=realpart(F1)
-        J=[epsilon(F1) epsilon(F(V,a,dual(b,1)))]
+        F1 = F(V,dual(a,1.),b)
+        p = realpart.(F1)
+        J=[epsilon.(F1) epsilon.(F(V,a,dual(b,1)))]
 
         an,bn=sort([a,b]-J\p)
         if isapprox(an,a) && isapprox(bn,b)
-            return Interval(an,bn)
+            return (an..bn)
         else
             a,b=an,bn
         end
     end
 
     warn("maxiterations reached")
-    Interval(a,b)
+    a..b
 end
 
 
 
 
-Fbounded(s::Bool,V,a,b)=emmoment(Fun(V,Interval(a,b))';bounded=s?(:right):(:left))-1
+Fbounded(s::Bool,V,a,b) = emmoment(Fun(V,(a..b))'; bounded= s ? (:right) : (:left))-1
 
-function equilibriummeasuresupportbounded(s::Bool,V,ab=Interval();maxiterations=100)
+function equilibriummeasuresupportbounded(s::Bool,V,ab=(-1.0..1.0);maxiterations=100)
     a,b=ab.a,ab.b
     for k=1:maxiterations
-        F1=s?Fbounded(s,V,a,dual(b,1.)):Fbounded(s,V,dual(a,1.),b)
+        F1=s ? Fbounded(s,V,a,dual(b,1.)) : Fbounded(s,V,dual(a,1.),b)
 
         p=realpart(F1)
         J=epsilon(F1)
@@ -107,26 +101,25 @@ function equilibriummeasuresupportbounded(s::Bool,V,ab=Interval();maxiterations=
         if s
             bn=b-J\p
             if isapprox(bn,b)
-                return Interval(a,bn)
+                return (a..bn)
             else
                 b=bn
             end
         else
             an=a-J\p
             if isapprox(an,a)
-                return Interval(an,b)
+                return (an..b)
             else
                 a=an
             end
         end
     end
     warn("maxiterations reached")
-    Interval(a,b)
+    a..b
 end
 
 # we assume that the zeroth coefficient is zero
-equilibriummeasure(V,gs...;bounded=:none,opts...)=-hilbertinverse(Fun(V,
-                                                                      equilibriummeasuresupport(V,gs...;
-                                                                                                bounded=bounded,opts...))';bounded=bounded)/(2π)
+equilibriummeasure(V,gs...;bounded=:none,opts...) =
+    -hilbertinverse(Fun(V,equilibriummeasuresupport(V,gs...;bounded=bounded,opts...))';bounded=bounded)/(2π)
 
 end #module
---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using EquilibriumMeasures, Plots
+
+μ = equilibriummeasure(x -> x^2)
+plot(μ)
